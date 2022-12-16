229f58573df14d0b3a1520a61c12153e57f3d4a5
J1min <aoiharu41@gmail.com>
2022-12-16 23:58:10 +0900

    Add: PUUID 데이터 추출

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import requests
-from pprint import pprint
+f = open('proNickname.txt', 'r')
 
-KEY = "RGAPI-88ac3bfa-6138-474a-9fe0-83d835caaa57"
-baseUrl = "https://kr.api.riotgames.com"
-
-leagues = [
-  "challengerleagues",
-  "grandmasterleagues",
-  # "masterleagues"
-]
-f = open("proNickname.txt", 'w')
-
-for league in leagues:
-  getProNicknameUrl = baseUrl + f"/lol/league/v4/{league}/by-queue/RANKED_SOLO_5x5?api_key={KEY}"
-  r = requests.get(getProNicknameUrl).json()
-  for response in r['entries']:
-    if response['veteran']:
-      f.write(response["summonerName"] + '\n')
-
+print('[', end=' ')
+while True:
+    line = f.readline()
+    if not line: break
+    print("'"+line+"', ", end=' ')
+print(']', end=' ')
 f.close()
