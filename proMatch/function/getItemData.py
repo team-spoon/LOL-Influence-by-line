61eb96209c1ce5cd0be5c81872d6cc483ed2f89c
J1min <aoiharu41@gmail.com>
2022-12-17 15:29:57 +0900

    Add: 패치별 아이템 변경점 json으로 return하는 함수 추가

---
+++ proMatch/function/getItemData.py
@@ -0,0 +1,14 @@
+import requests
+from pprint import pprint
+
+f = open('data.txt', 'w')
+getItemListUrl = 'https://ddragon.leagueoflegends.com/cdn/12.5.1/data/ko_KR/item.json'
+r = requests.get(getItemListUrl)
+
+pprint(r.json()['data'])
+
+for i in r.json()['data']:
+  pprint(r.json()['data'][i]['name'])
+
+
+f.close()
